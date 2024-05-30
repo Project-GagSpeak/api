4baa23a44c806d8f4a00346a9b636436c7d76a6c
Cordelia Mist <buzzmodetrix@gmail.com>
2024-05-30 12:04:46 -0700

    Testing out API system

---
+++ GagSpeakAPI/SignalR/IGagspeakHub.cs
@@ -0,0 +1,69 @@
+﻿using GagspeakSynchronos.API.Data.Enum;
+using GagspeakSynchronos.API.Dto;
+using GagspeakSynchronos.API.Dto.Group;
+using GagspeakSynchronos.API.Dto.User;
+
+namespace GagspeakSynchronos.API.SignalR;
+
+public interface IGagspeakHub
+{
+    const int ApiVersion = 30;
+    const string Path = "/Gagspeak";
+
+    Task<bool> CheckClientHealth();
+
+    Task Client_DownloadReady(Guid requestId);
+    Task Client_GroupChangePermissions(GroupPermissionDto groupPermission);
+    Task Client_GroupDelete(GroupDto groupDto);
+    Task Client_GroupPairChangeUserInfo(GroupPairUserInfoDto userInfo);
+    Task Client_GroupPairJoined(GroupPairFullInfoDto groupPairInfoDto);
+    Task Client_GroupPairLeft(GroupPairDto groupPairDto);
+    Task Client_GroupSendFullInfo(GroupFullInfoDto groupInfo);
+    Task Client_GroupSendInfo(GroupInfoDto groupInfo);
+    Task Client_ReceiveServerMessage(MessageSeverity messageSeverity, string message);
+    Task Client_UpdateSystemInfo(SystemInfoDto systemInfo);
+    Task Client_UserAddClientPair(UserPairDto dto);
+    Task Client_UserReceiveCharacterData(OnlineUserCharaDataDto dataDto);
+    Task Client_UserReceiveUploadStatus(UserDto dto);
+    Task Client_UserRemoveClientPair(UserDto dto);
+    Task Client_UserSendOffline(UserDto dto);
+    Task Client_UserSendOnline(OnlineUserIdentDto dto);
+    Task Client_UserUpdateOtherPairPermissions(UserPermissionsDto dto);
+    Task Client_UpdateUserIndividualPairStatusDto(UserIndividualPairStatusDto dto);
+    Task Client_UserUpdateProfile(UserDto dto);
+    Task Client_UserUpdateSelfPairPermissions(UserPermissionsDto dto);
+    Task Client_UserUpdateDefaultPermissions(DefaultPermissionsDto dto);
+    Task Client_GroupChangeUserPairPermissions(GroupPairUserPermissionDto dto);
+
+    Task<ConnectionDto> GetConnectionDto();
+
+    Task GroupBanUser(GroupPairDto dto, string reason);
+    Task GroupChangeGroupPermissionState(GroupPermissionDto dto);
+    Task GroupChangeOwnership(GroupPairDto groupPair);
+    Task<bool> GroupChangePassword(GroupPasswordDto groupPassword);
+    Task GroupClear(GroupDto group);
+    Task<GroupJoinDto> GroupCreate();
+    Task<List<string>> GroupCreateTempInvite(GroupDto group, int amount);
+    Task GroupDelete(GroupDto group);
+    Task<List<BannedGroupUserDto>> GroupGetBannedUsers(GroupDto group);
+    Task<GroupJoinInfoDto> GroupJoin(GroupPasswordDto passwordedGroup);
+    Task<bool> GroupJoinFinalize(GroupJoinDto passwordedGroup);
+    Task GroupLeave(GroupDto group);
+    Task GroupRemoveUser(GroupPairDto groupPair);
+    Task GroupSetUserInfo(GroupPairUserInfoDto groupPair);
+    Task<List<GroupFullInfoDto>> GroupsGetAll();
+    Task GroupUnbanUser(GroupPairDto groupPair);
+    Task<int> GroupPrune(GroupDto group, int days, bool execute);
+
+    Task UserAddPair(UserDto user);
+    Task UserDelete();
+    Task<List<OnlineUserIdentDto>> UserGetOnlinePairs(CensusDataDto? censusDataDto);
+    Task<List<UserFullPairDto>> UserGetPairedClients();
+    Task<UserProfileDto> UserGetProfile(UserDto dto);
+    Task UserPushData(UserCharaDataMessageDto dto);
+    Task UserRemovePair(UserDto userDto);
+    Task UserReportProfile(UserProfileReportDto userDto);
+    Task UserSetProfile(UserProfileDto userDescription);
+    Task UserUpdateDefaultPermissions(DefaultPermissionsDto defaultPermissionsDto);
+    Task SetBulkPermissions(BulkPermissionsDto dto);
+}
