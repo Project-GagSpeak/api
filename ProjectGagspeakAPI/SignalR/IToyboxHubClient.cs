9cda7a52b81836ce188cd81fe15b096e7c17e4f6
Cordelia Mist <buzzmodetrix@gmail.com>
2024-07-26 12:38:26 -0700

    I didnt forget to update my HubClientInterface i swear

--- ProjectGagspeakAPI/SignalR/IToyboxHubClient.cs
+++ ProjectGagspeakAPI/SignalR/IToyboxHubClient.cs
@@ -14,11 +14,12 @@ public interface IToyboxHubClient : IToyboxHub
 {
     void OnReceiveToyboxServerMessage(Action<MessageSeverity, string> act); // send message to client
     void OnUserReceiveRoomInvite(Action<RoomInviteDto> act); // Receives a room invite from another user.
-    void OnUserJoinedRoom(Action<RoomInfoDto> act); // whenever you joined a room.
-    void OnOtherUserJoinedRoom(Action<RoomParticipantDto> act); // Recieved when another user joins the room.
-    void OnOtherUserLeftRoom(Action<RoomParticipantDto> act); // Recieved when another user leaves the room.
-    void OnUserReceiveRoomMessage(Action<RoomMessageDto> act); // Recieves a message from another user in the room.
-    void OnUserReceiveDeviceInfo(Action<UserCharaDeviceInfoMessageDto> act); // Receives device info from another connected user.
-    void OnUserDeviceUpdate(Action<UpdateDeviceDto> act); // Updates the clients device with the new update.
-    void OnReceiveRoomClosedMessage(Action<string> act); // Informs the client that the room has been closed.
+    void OnPrivateRoomJoined(Action<RoomInfoDto> act); // whenever you joined a room.
+    void OnPrivateRoomOtherUserJoined(Action<RoomParticipantDto> act); // Recieved when another user joins the room.
+    void OnPrivateRoomOtherUserLeft(Action<RoomParticipantDto> act); // Recieved when another user leaves the room.
+    void OnPrivateRoomUpdateUser(Action<RoomParticipantDto> act);
+    void OnPrivateRoomMessage(Action<RoomMessageDto> act); // Recieves a message from another user in the room.
+    void OnPrivateRoomReceiveUserDevice(Action<UserCharaDeviceInfoMessageDto> act); // Receives device info from another connected user.
+    void OnPrivateRoomDeviceUpdate(Action<UpdateDeviceDto> act); // Updates the clients device with the new update.
+    void OnPrivateRoomClosed(Action<string> act); // Informs the client that the room has been closed.
 }
