eec1c0c8da3b3f27b2e1bcd51bbbca4f7233ee30
Cordelia Mist <buzzmodetrix@gmail.com>
2024-08-05 18:41:26 -0700

    fix for timer

--- ProjectGagspeakAPI/Dto/Connection/ConnectionDto.cs
+++ ProjectGagspeakAPI/Dto/Connection/ConnectionDto.cs
@@ -32,6 +32,7 @@ public record ConnectionDto(UserData User)
     public string WardrobeActiveSetAssigner { get; set; } = ""; // person who Enabled the set.
     public bool WardrobeActiveSetLocked { get; set; } = false; // the lock status of the user's active outfit
     public string WardrobeActiveSetLockAssigner { get; set; } = ""; // person who Locked the set.
+    public DateTimeOffset WardrobeActiveSetLockTime { get; set; } = DateTimeOffset.MinValue; // when the locked set will expire
     public string ToyboxActivePatternName { get; set; } = ""; // the name of the user's actively running pattern
 
 }
