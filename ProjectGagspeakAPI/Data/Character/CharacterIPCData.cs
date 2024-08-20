661322d7c16ec5f678fed1d59fad732a24b21aab
Cordelia Mist <buzzmodetrix@gmail.com>
2024-08-20 09:45:08 -0700

    idk why it hid this lol.

--- ProjectGagspeakAPI/Data/Character/CharacterIPCData.cs
+++ ProjectGagspeakAPI/Data/Character/CharacterIPCData.cs
@@ -20,7 +20,18 @@ public class CharacterIPCData
     /// <summary>
     /// The Pair's Latest known Status Manager Data (what moodles are actively applied)
     /// </summary>
-//    public string MoodlesStatusManagerData { get; set; } = string.Empty;
     public string MoodlesData { get; set; } = string.Empty;
+
+    /// <summary>
+    /// A list of the player's Moodles Statuses.
+    /// </summary>
+    public List<MoodlesStatusInfo> MoodlesStatuses { get; set; } = new();
+
+    /// <summary>
+    /// A dictionary of the players Moodle Presets, along with the associated Moodles Statuses that come with them.
+    /// 
+    /// TODO: Consider narrowing this down to a list of GUID's. As all the status info's should be in the moodlesStatuses list.
+    /// </summary>
+    public List<(Guid, List<Guid>)> MoodlesPresets { get; set; } = new();
 
 }
