172b72612d642e9cf3ad004036a7bbc15aeaf3d3
Cordelia Mist <buzzmodetrix@gmail.com>
2024-08-23 12:00:16 -0700

    Api Bump

--- ProjectGagspeakAPI/Data/Character/CharacterIPCData.cs
+++ ProjectGagspeakAPI/Data/Character/CharacterIPCData.cs
@@ -6,32 +6,36 @@ namespace GagspeakAPI.Data.Character;
 /// Stores Information about the Player's Moodles & Customize+ Data.
 /// 
 /// FOR MOODLES:
-/// 
-/// The Data contains the following.
-/// 
-/// 
-/// 
-/// 
-/// 
+/// - Stores Status Manager String
+/// - Stores all active Moodle Statuses on the player, with the status info.
+/// - Stores the GUID's of their Presets and the associated Moodles Statuses of those presets by GUID.
 /// </summary>
 [MessagePackObject(keyAsPropertyName: true)]
 public class CharacterIPCData
 {
-    /// <summary>
-    /// The Pair's Latest known Status Manager Data (what moodles are actively applied)
-    /// </summary>
+    /// <summary> Last Known StatusManager compressed in base64 string. (for easy setting) </summary>
     public string MoodlesData { get; set; } = string.Empty;
 
-    /// <summary>
-    /// A list of the player's Moodles Statuses.
-    /// </summary>
+    /// <summary> The list of Active Statuses in the StatusManager, non-serialized. </summary>
+    public List<MoodlesStatusInfo> MoodlesDataStatuses { get; set; } = new();
+
+    /// <summary> A list of the player's Statuses. </summary>
     public List<MoodlesStatusInfo> MoodlesStatuses { get; set; } = new();
 
-    /// <summary>
-    /// A dictionary of the players Moodle Presets, along with the associated Moodles Statuses that come with them.
-    /// 
-    /// TODO: Consider narrowing this down to a list of GUID's. As all the status info's should be in the moodlesStatuses list.
-    /// </summary>
+    /// <summary> List of player's Presets, and the Status GUID's the preset contains </summary>
     public List<(Guid, List<Guid>)> MoodlesPresets { get; set; } = new();
 
+    /// <summary> Equals Override to ensure things are the same. </summary>
+    public override bool Equals(object? obj)
+    {
+        // return false if object is not characterIPCData
+        if (obj is CharacterIPCData data)
+        {
+            return MoodlesData == data.MoodlesData 
+                && MoodlesDataStatuses.SequenceEqual(data.MoodlesDataStatuses)
+                && MoodlesStatuses.SequenceEqual(data.MoodlesStatuses)
+                && MoodlesPresets.SequenceEqual(data.MoodlesPresets);
+        }
+        return false;
+    }
 }
