66bc6f49c05f8e20a5c0f77d068b099869a4622b
Cordelia Mist <buzzmodetrix@gmail.com>
2024-09-13 19:41:42 -0700

    changes to routing

--- ProjectGagspeakAPI/Routes/GagspeakPiShock.cs
+++ ProjectGagspeakAPI/Routes/GagspeakPiShock.cs
@@ -2,9 +2,10 @@ namespace GagspeakAPI.Routes;
 
 public class GagspeakPiShock
 {
-    public const string BaseUri = "https://do.pishock.com/api";
-    public const string PiShock_GetInfo = "/GetShockerInfo"; 
-    public const string PiShock_Execute = "/apioperate";
-    public static Uri GetInfoPath() => new Uri(new Uri(BaseUri), PiShock_GetInfo);
-    public static Uri ExecuteOperationPath() => new Uri(new Uri(BaseUri), PiShock_Execute);
+    public const string BaseUri = "https://do.pishock.com";
+    public const string Api = "api";
+    public const string PiShock_GetInfo = "GetShockerInfo"; 
+    public const string PiShock_Execute = "apioperate";
+    public static Uri GetInfoPath() => new Uri(new Uri(BaseUri), Api + "/" + PiShock_GetInfo);
+    public static Uri ExecuteOperationPath() => new Uri(new Uri(BaseUri), Api + "/" + PiShock_Execute);
 }
