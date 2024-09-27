b6d0e1d2339903af4704ac1022468f13f3588c89
Cordelia Mist <buzzmodetrix@gmail.com>
2024-09-26 21:16:43 -0700

    Some api additions

---
+++ ProjectGagspeakAPI/Enums/ClientEnums/DeepDungeonType.cs
@@ -0,0 +1,8 @@
+namespace GagspeakAPI.Enums;
+public enum DeepDungeonType
+{
+    Unknown,
+    PalaceOfTheDead,
+    HeavenOnHigh,
+    EurekaOrthos,
+}
---
+++ ProjectGagspeakAPI/Enums/ClientEnums/JobRoleName.cs
@@ -0,0 +1,9 @@
+namespace GagspeakAPI.Enums;
+public enum ActionRoles : byte
+{
+    NonCombat = 0,
+    Tank = 0x1,
+    MeleeDps = 0x2,
+    RangedDps = 0x3,
+    Healer = 0x4
+}
