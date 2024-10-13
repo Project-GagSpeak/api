457eb589b965678fdda346cb1c94284958c23802
Cordelia Mist <buzzmodetrix@gmail.com>
2024-10-13 11:28:45 -0700

    Api Server-Side syncronization for changes.

--- ProjectGagspeakAPI/Enums/SharedEnums/Padlocks.cs
+++ ProjectGagspeakAPI/Enums/SharedEnums/Padlocks.cs
@@ -15,5 +15,7 @@ public enum Padlocks
     TimerPasswordPadlock,   // Timer Password Padlock, must enter password to unlock, but only after a certain amount of time
     OwnerPadlock,           // Can only be locked/unlocked by people with Owner Padlock perm Access
     OwnerTimerPadlock,      // Can only be locked/unlocked by people with Owner Padlock perm Access (Timed)
-    MimicPadlock,           // Mimic Padlock, a Timed Padlock that Cannot be unlocked.
+    DevotionalPadlock,      // Can only be unlocked by the same person who locked it. (or safeword)
+    DevotionalTimerPadlock, // Can only be unlocked by the same person who locked it. (or safeword) (Timed)
+    MimicPadlock,           // Mimic Padlock, a Timed Padlock that Cannot be unlocked. (Only Applied by Mimics)
 };
