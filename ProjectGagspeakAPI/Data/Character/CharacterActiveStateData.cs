d672610d47ca9ae73b9bc7adfe72eb7cb8b6593a
Cordelia Mist <buzzmodetrix@gmail.com>
2024-10-21 13:03:46 -0700

    api

--- ProjectGagspeakAPI/Data/Character/CharacterActiveStateData.cs
+++ ProjectGagspeakAPI/Data/Character/CharacterActiveStateData.cs
@@ -9,19 +9,14 @@ namespace GagspeakAPI.Data.Character;
 [MessagePackObject(keyAsPropertyName: true)]
 public record CharacterActiveStateData
 {
-	public string WardrobeActiveSetName { get; set; } = ""; // the name of the user's active outfit
-	public string WardrobeActiveSetAssigner { get; set; } = ""; // person who Enabled the set.
+    public Guid ActiveSetId { get; set; } = Guid.Empty; // the ID of the user's active outfit
+	public string ActiveSetName { get; set; } = ""; // the name of the user's active outfit
+	public string ActiveSetEnabler { get; set; } = ""; // person who Enabled the set.
     public string Padlock { get; set; } = Padlocks.None.ToName(); // Type of padlock used to lock the set.
     public string Password { get; set; } = ""; // password bound to the set's lock type.
     public DateTimeOffset Timer { get; set; } = DateTimeOffset.UtcNow; // timer placed on the set's lock
     public string Assigner { get; set; } = ""; // UID that locked the set.
 
 	/* User's ToyboxData state references */
-	public Guid ToyboxActivePatternName { get; set; } = Guid.Empty; // the name of the user's actively running pattern
-
-    /* User's Hardcore state references */
-    public string ForcedFollowApplierUID { get; set; } = "";
-    public string ForcedSitApplierUID { get; set; } = "";
-    public string ForcedStayApplierUID { get; set; } = "";
-    public string ForcedBlindfoldAssignerUID { get; set; } = "";
+	public Guid ActivePatternName { get; set; } = Guid.Empty; // the name of the user's actively running pattern
 }
