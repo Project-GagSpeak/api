aac76adc737705d4335416884a48f5b0271d29d2
Cordelia Mist <buzzmodetrix@gmail.com>
2025-04-05 13:24:10 -0700

    Changes to connection Dto for account verification checking.

--- ProjectGagspeakAPI/Dto/Connection/ConnectionDto.cs
+++ ProjectGagspeakAPI/Dto/Connection/ConnectionDto.cs
@@ -8,7 +8,7 @@ namespace GagspeakAPI.Dto.Connection;
 /// <summary> The information sent to a client whenever they successfully connect to GagSpeak Servers. </summary>
 /// <remarks> All Synced Data Information is provided here. The client is responsible for keeping it synced. </remarks>
 [MessagePackObject(keyAsPropertyName: true)]
-public record ConnectionDto(UserData User)
+public record ConnectionDto(UserData User, bool IsVerified)
 {
     public Version CurrentClientVersion { get; set; } = new(0, 0, 0);
     public int ServerVersion { get; set; }
@@ -20,9 +20,6 @@ public record ConnectionDto(UserData User)
     public List<PublishedPattern> PublishedPatterns { get; init; } = new();
     public List<PublishedMoodle> PublishedMoodles { get; init; } = new();
 
-    /// <summary> Helps us know when an account was removed via discord. </summary>
-    public List<string> ActiveAccountUidList { get; init; } = new();
-
-    /// <summary> Base64 Achievement Data string stored on the server. </summary>
-    public string UserAchievements { get; set; } = string.Empty;
+    public List<string> ActiveAccountUidList { get; init; } = new(); // All Auth Uids associated with the connected user.
+    public string UserAchievements { get; set; } = string.Empty; // Base64 string of the achievement data
 }
