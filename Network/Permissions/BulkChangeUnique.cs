7a3c973d35e92545bdefb1cad134b0a94c18e5c4
Cordelia Mist <buzzmodetrix@gmail.com>
2025-07-28 16:21:07 -0700

    fix bulk unique change.

--- Network/Permissions/BulkChangeUnique.cs
+++ Network/Permissions/BulkChangeUnique.cs
@@ -1,15 +1,13 @@
 using GagspeakAPI.Data;
 using GagspeakAPI.Data.Permissions;
+using GagspeakAPI.Enums;
 using MessagePack;
 
 namespace GagspeakAPI.Network;
 
 /// <summary>
-///     Updates all PairPerms and UserPairPermissionAccess for the <paramref name="User"/>'s end, to the new permissions.
-/// </summary>
-/// <remarks>
-///     If used in a callback, the <paramref name="User"/> is <u>the Kinkster that made the change</u>,
-///     if used in a server call, <paramref name="User"/> is <u>the target Kinkster to make the changes to</u>.
+///     Updates all PairPerms and UserPairPermissionAccess for
+///     the <paramref name="User"/>'s end, to the new permissions.
 /// </remarks>
 [MessagePackObject(keyAsPropertyName: true)]
-public record BulkChangeUnique(UserData User, PairPerms NewPerms, PairPermAccess NewAccess) : KinksterBase(User);
+public record BulkChangeUnique(UserData User, PairPerms NewPerms, PairPermAccess NewAccess, UpdateDir Direction, UserData Enactor) : KinksterBase(User);
