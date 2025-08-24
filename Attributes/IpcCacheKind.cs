910ea5166da62b80b150421018cd5a2a9e618629
Cordelia Mist <buzzmodetrix@gmail.com>
2025-08-24 14:35:20 -0700

    update cacheKind order

--- Attributes/IpcCacheKind.cs
+++ Attributes/IpcCacheKind.cs
@@ -10,8 +10,8 @@ public enum DataSyncKind : byte
 
     Glamourer   = 1 << 0, // byte (1)
     ModManips   = 1 << 1, // byte (2)
-    CPlus       = 1 << 2, // byte (4)
-    Heels       = 1 << 3, // byte (8)
+    Heels       = 1 << 2, // byte (4)
+    CPlus       = 1 << 3, // byte (8)
     Honorific   = 1 << 4, // byte (16)
     PetNames    = 1 << 5, // byte (32)
 }
